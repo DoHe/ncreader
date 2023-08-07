cd51a9cdc9a1d87a04883e90ce5a6ce551c5b871
dohe <wahlholz@gmx.de>
2023-08-07 11:39:08 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {
+  StyleSheet, Text, View, Button, FlatList, ActivityIndicator, Image, Platform,
+} from 'react-native';
+import { WebView } from 'react-native-webview';
+import React, { useEffect, useState } from 'react';
+import mock from './mock';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +14,74 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const mocked = true;
+
+export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      if (mocked) {
+        setData(mock.Comics);
+      } else {
+        const response = await fetch('https://reactnative.dev/movies.json');
+        const json = await response.json();
+        setData(json.movies);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text>My texts 2</Text>
+      <Button
+        onPress={() => {
+          console.log('You tapped the button!');
+        }}
+        title="Press Me"
+      />
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <View>
+              <Image
+                source={{ uri: item.favicon }}
+                style={{ width: 16, height: 16 }}
+              />
+              <Text>
+                {item.title}
+              </Text>
+              {Platform.OS === 'web' ? (
+                <div dangerouslySetInnerHTML={{ __html: item.body }} />
+              ) : (
+                <WebView
+                  originWhitelist={['*']}
+                  source={{ html: item.body }}
+                  style={{
+                    backgroundColor: 'ff0000',
+                    width: 100,
+                    height: 100,
+                  }}
+                />)}
+            </View>
+          )}
+        />
+      )}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
---
+++ mock.js
@@ -0,0 +1,20 @@
+export default {
+  IT: [
+    {
+      id: 1,
+      title: 'Ray-Ban Stories: Metas Kamera-Sonnebrille wird kaum benutzt',
+      body: 'Nur zehn Prozent der Käufer von Metas Ray-Ban Stories sollen die Kamera-Sonnenbrille aktiv benutzen. Dennoch will Meta einen Nachfolger bringen.',
+      favicon: 'http://rss.golem.de/favicon.ico',
+      url: 'https://www.golem.de/news/ray-ban-stories-metas-kamera-sonnebrille-wird-kaum-benutzt-2308-176504.html',
+    },
+  ],
+  Comics: [
+    {
+      id: 2,
+      title: 'Wilde Life - 1231',
+      body: '<a target="_blank" rel="noreferrer" href="https://www.wildelifecomic.com/comic/1231"><img src="https://www.wildelifecomic.com/comicsthumbs/1691384648-1231.png" alt="1691384648-1231.png"><br>New comic!</a><br>Today\'s News:<br>',
+      favicon: 'https://www.wildelifecomic.com/favicon.ico',
+      url: 'https://www.wildelifecomic.com/comic/1231',
+    },
+  ],
+};
